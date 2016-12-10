80864bb08543e5f71d16e3fd4ed930afe4756e2a
Norbert Szucs <szucs.norbert@outlook.com>
2016-12-10 21:05:22 +0100

    Fix CORS

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const app = express();
 
 // Do not allow cors in dev?!
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Credentials", 'true');
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE');
