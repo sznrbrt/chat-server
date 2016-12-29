019b2f4d60cb0d63eee6379153e79eca2b85c68e
Norbert Szucs <szucs.norbert@outlook.com>
2016-12-29 15:31:54 +0100

    Add CLIENT_URL as a CORS header input

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ mongoose.connect(MONGOURL,  options, (err) => {
 const app = express();
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL || "http://localhost:3000");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Credentials", 'true');
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE');
